df1ba3f557b8de64b68d31b4f7f6e9649a1a1705
Justin Chen <jchen978@gmail.com>
2014-12-15 17:18:48 -0600

    haha

--- Jac_save_all.py
+++ Jac_save_all.py
@@ -5,15 +5,18 @@ Script for running a fit for all the solutions at different temperatures
 import numpy as np
 import os
 
+def run_saving():
+    import analysis_scripts.Jac_save_module as jsm
+    jsm.run_main()
 
 if __name__ == "__main__":
 
     temps = np.loadtxt("Temparray.txt")
     cwd = os.getcwd()
+    print temps
     for t in temps:
         print "Starting temperature %d" % t
-        os.chdir(t)
-        import analysis_scripts.Jac_save_module as jsm
-        jsm.run_main()
+        os.chdir(str(t))
+        run_saving()
         os.chdir(cwd)
     print "Finished all temperatures"
