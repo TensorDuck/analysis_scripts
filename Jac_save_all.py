8b01db10fb5d6d933773ee656bcd58a26950d872
Justin Chen <jchen978@gmail.com>
2015-01-09 16:10:01 -0600

    bugs

--- Jac_save_all.py
+++ Jac_save_all.py
@@ -13,17 +13,20 @@ if __name__ == "__main__":
 
     temps = np.loadtxt("Temparray.txt")
     cwd = os.getcwd()
-    f = open("Fitting_damping.txt","w")
+    fit_string = ""
     print temps
     for t in temps:
         print "Starting temperature %d" % t
         os.chdir("%d"%t)
         iteration = run_saving(int(t))
-        ffit = int(open("1PB7/iteration_%d/newton/fitting_scale").readline())
+        ffit = int(open("1PB7/iteration_%d/newton/fitting_scale"%iteration).readline())
         if ffit == "1":
-            f.write("Temperature %d scaled = True\n"%t)
+            fit_string += "Temperature %d scaled = True\n"%t
         else:
-            f.write("Temperature %d scaled = False\n"%t)
+            fit_string += "Temperature %d scaled = False\n"%t
         os.chdir(cwd)
+    
+    f = open("Fitting_damping_%d.txt"%iteration,"w")
+    f.write("fit_string")
     f.close()
     print "Finished all temperatures"
