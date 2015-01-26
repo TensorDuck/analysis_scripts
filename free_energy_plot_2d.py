9418bc32898d91d4aadea1ef66bc559e54fb5553
Justin Chen <jchen978@gmail.com>
2015-01-26 15:19:51 -0600

    bugs

--- free_energy_plot_2d.py
+++ free_energy_plot_2d.py
@@ -39,6 +39,8 @@ def handle_dmdmd(ext1, ext2, args):
             fit_range = np.arange(step, stop, step)
         else:
             fit_range = np.arange(start+step, stop, step)
+        if not stop==fit_range(np.shape(fit_range)[0]):
+            fit_range = np.append(fit_range, stop)
         ##Do first step, it's unique
         rc1, rc2, weights = dmdmd_iteration(start, step, weights, wsum, rc1, rc2, ext1, ext2, cfd)
         plot_2D_Free_Energy(rc1, rc2, rc1n, rc2n, "iter%d-%d"%(start,step), args, weights=weights, temp=args.temps[0])
