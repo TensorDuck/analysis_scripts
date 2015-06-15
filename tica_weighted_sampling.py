8f35238c87cf005a7e41aca13f83d9e54ddfe2cf
Justin Chen <jchen978@gmail.com>
2015-06-15 18:23:04 -0500

    added test script for weigthed sampling for TICA

---
+++ tica_weighted_sampling.py
@@ -0,0 +1,60 @@
+""" Script for calculating Tica from a weighted set of points
+Does so by taking a random sampling of the points
+"""
+
+import numpy as np
+import pyemma
+import pyemma.coordinates as coor
+import mdtraj as md
+import time
+import analysis_scripts.plot_package as pltpkg
+import argparse
+
+
+def run_sampling(args):
+    topology = "Native.pdb"
+    ticadim = 10
+    num_sample_frames = 10000
+    
+    fn = args.file #file name
+    wn = args.weights #weights name
+    
+    weights = np.loadtxt(wn)
+    weights = weights/np.sum(weights)
+    
+    feat = coor.featurizer(topology)
+    feat.add_distances_ca()
+    X1 = coor.load(fn, feat, stride=1)
+    
+    sampled_frames=np.array((num_sample_frames,np.shape(X1)[1]))
+    
+    selected_frames = np.random.choice(np.shape(X1)[0], size=num_sample_frames, replace=True, p=weights)
+    
+    for i in range(num_sample_frames):
+        sampled_frames[i,:] = X1[selected_frames[i],:]
+    
+    
+    tica_obj = coor.tica(sampled_frames, stride=1, lag=i, dim=ticadim)
+    outputs = tica_obj.get_output()[0]
+    eigen = tica_obj.eigenvalues
+    np.savetxt("output.dat", outputs)
+    np.savetxt("eigenvalues.dat", eigen)
+
+        
+    
+
+def get_args():
+    parser = argparse.ArgumentParser(description="parent set of parameters", add_help=False)
+    parser.add_argument("--file", type=str, help="File, either .gro or .xtc")
+    parser.add_argument("--weights", type=str, help="File, .w")
+    args = parser.parse_args()
+    
+    return args
+    
+
+if __name__ == "__main__":
+    args = get_args()
+    run_sampling(args)
+
+
+
