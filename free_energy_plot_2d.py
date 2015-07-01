23d2e54942de3082e83be49f8f8760c1468cd8e4
Justin Chen <jchen978@gmail.com>
2015-06-30 22:48:02 -0500

    bugs

--- free_energy_plot_2d.py
+++ free_energy_plot_2d.py
@@ -141,7 +141,7 @@ def plot_2D_Free_Energy(rc1, rc2, rc1n, rc2n, name, args, weights=None, temp=300
         x, y, z = hist2d.make(rc1, rc2, nbins, nbins, temperature=temp, weight=weights, plot_style=plot_style, free_energy_plot=True, idx_smoothing=1)
         cp = plt.scatter(x, y, s=10, c=z, marker='o', linewidth=0.)
         if not extra == None:
-            plt.scatter(extra[:,0], extra[:,1], 'ok')
+            plt.plot(extra[:,0], extra[:,1], "ok", markersize=1)
         #vmax=7
         if not axis == None:
             plt.axis(axis)
@@ -165,7 +165,7 @@ def plot_2D_Free_Energy(rc1, rc2, rc1n, rc2n, name, args, weights=None, temp=300
         cp = plt.contourf(xc, yc, zc, 20)
         plt.contour(xc, yc, zc, cp.levels, colors='k', hold='on')
         if not extra == None:
-            plt.scatter(extra[:,0], extra[:,1], 'ok')
+            plt.plot(extra[:,0], extra[:,1], "ok", markersize=1)
         if not axis == None:
             plt.axis(axis)
         plt.xlabel(rc1n, size=16)
