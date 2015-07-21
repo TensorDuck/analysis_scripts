e731ffd6ca9db578cd46b13db34aa81630c351fc
Justin Chen <jchen978@gmail.com>
2015-07-21 17:42:03 -0500

    bugs

--- JacRunModule.py
+++ JacRunModule.py
@@ -56,7 +56,7 @@ def run_calc_all(args):
             os.chdir(args.cwd)
             fret_data = pdistance.get_FRET_data(args.fret_data)
             os.chdir(owd)
-            pdistance.plot_iterations(centers_of_bins, normalized_valu, args.pairs, labels, args.spacing, t, fretdata=args.fret_data)
+            pdistance.plot_iterations(centers_of_bins, normalized_valu, args.pairs, labels, args.spacing, t, fretdata=fret_data)
             os.chdir(args.cwd)
             #write the cutoff into a file for easy access
             fsv.write("The Cutoff occurs between singular values: %.3e and %.3e\n" % (highvalue, lowvalue))
